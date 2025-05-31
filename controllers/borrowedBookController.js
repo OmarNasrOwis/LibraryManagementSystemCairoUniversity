00f2ab4af5ff9e8404b6b67cec7f521ba9d2c91e
Omar Nasr <ramorsan2013@gmail.com>
2025-05-31 09:32:25 +0300

    join tables to add required data in borrowed books requests

--- controllers/borrowedBookController.js
+++ controllers/borrowedBookController.js
@@ -1,11 +1,19 @@
 import pool from "../config/db.js";
 
 export const getAllBorrowedBooks = async () => {
-  const result = await pool.query("SELECT * FROM borrowed_books");
+  const result = await pool.query(`
+    SELECT borrowed_books.*,books.*
+    FROM borrowed_books
+    JOIN books ON borrowed_books.isbn = books.isbn
+  `);
   return result.rows;
 };
 
-export const createBorrowRequest = async (student_id, isbn, request_start_date) => {
+export const createBorrowRequest = async (
+  student_id,
+  isbn,
+  request_start_date
+) => {
   const result = await pool.query(
     "INSERT INTO borrowed_books (studentid, isbn, status, request_start_date) VALUES ($1, $2, $3, $4) RETURNING *",
     [student_id, isbn, "0", request_start_date]
@@ -13,39 +21,50 @@ export const createBorrowRequest = async (student_id, isbn, request_start_date) 
   return result.rows[0];
 };
 
-
 export const getBorrowedBooksByStudent = async (student_id) => {
-  return await pool.query("SELECT * FROM borrowed_books WHERE studentid = $1", [student_id]);
+  return await pool.query(
+    `
+    SELECT borrowed_books.*, books.*
+    FROM borrowed_books
+    JOIN books ON borrowed_books.isbn = books.isbn
+    WHERE borrowed_books.studentid = $1
+  `,
+    [student_id]
+  );
 };
 
 export const processBorrowDecision = async (request_id, status) => {
-  return await pool.query("UPDATE borrowed_books SET status = $1 WHERE id = $2 RETURNING *", [status, request_id]);
+  return await pool.query(
+    "UPDATE borrowed_books SET status = $1 WHERE id = $2 RETURNING *",
+    [status, request_id]
+  );
 };
-
 export const getPendingBorrows = async () => {
   return await pool.query(`
-    SELECT borrowed_books.*, users.fullname
+    SELECT borrowed_books.*, books.*, users.fullname
     FROM borrowed_books
     JOIN users ON borrowed_books.studentid = users.studentid
+    JOIN books ON borrowed_books.isbn = books.isbn
     WHERE borrowed_books.status = 2
   `);
 };
 
-
 export const getApprovedBorrows = async () => {
   return await pool.query(`
-    SELECT borrowed_books.*, users.fullname
+    SELECT borrowed_books.*, books.*, users.fullname
     FROM borrowed_books
     JOIN users ON borrowed_books.studentid = users.studentid
+    JOIN books ON borrowed_books.isbn = books.isbn
     WHERE borrowed_books.status = 1
   `);
 };
 
 export const getRejectedBorrows = async () => {
   return await pool.query(`
-    SELECT borrowed_books.*, users.fullname
+    SELECT borrowed_books.*, books.*, users.fullname
     FROM borrowed_books
     JOIN users ON borrowed_books.studentid = users.studentid
+    JOIN books ON borrowed_books.isbn = books.isbn
     WHERE borrowed_books.status = 0
   `);
 };
